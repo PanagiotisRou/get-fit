c7be003735f9017b37ae7e509e82bdb2891a9a59
PanagiotisRou <panagneu@web.de>
2020-08-26 20:20:22 +0200

    changes

--- js/custom.js
+++ js/custom.js
@@ -45,6 +45,8 @@ $(document).ready(function() {
         loop: false,
         nav: true,
         dots: false,
+        mouseDrag: false,
+        touchDrag: false,
         navText: ['<span class="fas fa-chevron-left fa-2x"></span>','<span class="fas fa-chevron-right fa-2x"></span>']
   }
   );
